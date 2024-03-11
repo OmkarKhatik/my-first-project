c9bc1e8a476eceb63a67ef136c424c1629bb2ce4
Omkar khatik <Omkar khatik@omkar-MOQNAA36>
2024-03-11 08:39:37 +0530

    commit it

---
+++ JavaProject/src/newjavapractice/Hello.java
@@ -0,0 +1,19 @@
+package newjavapractice;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str);
+		
+		System.out.println();
+		
+		String str1 = sc.next();
+		System.out.println(str1);
+		
+	}
+
+}
